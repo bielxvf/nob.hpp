c87684b888990cfa8f0ddadea9ad2ceeaf58ea18
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-15 14:14:47 +0200

    Adding some functions for file download and extraction, not quite done yet

--- nob.hpp
+++ nob.hpp
@@ -40,6 +40,12 @@ enum class LogLevel {
     Error,
 };
 
+enum class Verbosity {
+    Quiet,
+    Quieter,
+    Verbose,
+};
+
 template<typename... Args>
 void log(LogLevel lvl, Args&&... args)
 {
@@ -200,8 +206,8 @@ void go_rebuild_urself(int argc, char** argv, fs::path source_path)
 {
     auto binary_path = get_executable_path();
 
-    auto binary_time = std::filesystem::last_write_time(binary_path);
-    auto source_time = std::filesystem::last_write_time(source_path);
+    auto binary_time = fs::last_write_time(binary_path);
+    auto source_time = fs::last_write_time(source_path);
 
     if (source_time > binary_time) {
         log(LogLevel::Info, "Rebuilding meself");
@@ -215,5 +221,193 @@ void go_rebuild_urself(int argc, char** argv, fs::path source_path)
     }
 }
 
+bool download(const std::string& url,
+              std::optional<fs::path> out = std::nullopt,
+              std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("curl",
+            "-L" /* Follow redirects */
+    );
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            case Verbosity::Quiet: { cmd.add("-s"); } break;
+            default: {} break;
+        }
+    }
+
+    if (out_path) {
+        cmd.add("-o", out->string());
+    } else {
+        cmd.add("-O"); /* Save to whatever name is in the Content-Disposition header or the last part of the URL */
+    }
+    cmd.add(url);
+
+    return cmd.run_sync() == 0;
+}
+
+bool extract_tar_gz(const fs::path& archive,
+                    std::optional<fs::path> out = std::nullopt,
+                    std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("tar",
+            "-x",
+            "-z"
+    );
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            default: {} break;
+        }
+    }
+
+    cmd.add("-f", archive.string());
+
+    if (out) {
+        cmd.add("-C", out->string());
+    }
+
+    return cmd.run_sync() == 0;
+}
+
+bool extract_tar_bz2(const fs::path& archive,
+                     std::optional<fs::path> out = std::nullopt,
+                     std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("tar",
+            "-x", /* Extract */
+            "-j"  /* Use bz2 decompression */
+    );
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            default: {} break;
+        }
+    }
+
+    cmd.add("-f", archive.string());
+
+    if (out) {
+        cmd.add("-C", out->string());
+    }
+
+    return cmd.run_sync() == 0;
+}
+
+bool extract_bz2(const fs::path& compressed,
+                 std::optional<fs::path> out = std::nullopt,
+                 std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("bzip2",
+            "-d"); /* Decompress */
+
+    if (out) {
+        cmd.add("-c"); /* To stdout */
+    }
+
+    cmd.add("-k", compressed.string()); /* Keep original */
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            case Verbosity::Quiet: { cmd.add("-q"); } break;
+            default: {} break;
+        }
+    }
+
+    if (out) {
+        cmd.add(">", out->string()); /* Redirect to chosen file */
+    }
+
+    return cmd.run_sync() == 0;
+}
+
+bool extract_zip(const fs::path& archive,
+                 std::optional<fs::path> out = std::nullopt,
+                 std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("unzip");
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            case Verbosity::Quiet: { cmd.add("-q"); } break;
+            case Verbosity::Quieter: { cmd.add("-qq"); } break;
+            default: {} break;
+        }
+    }
+
+    cmd.add(archive.string());
+
+    if (out) {
+            cmd.add("-d", out->string()); /* Redirect to chosen file */
+    }
+
+    return cmd.run_syns() == 0;
+}
+
+bool extract_gz(const fs::path& compressed,
+                std::optional<fs::path> out = std::nullopt,
+                std::optional<Verbosity> v = std::nullopt)
+{
+    Cmd cmd("gunzip");
+
+    if (v) {
+        switch (*v) {
+            case Verbosity::Verbose: { cmd.add("-v"); } break;
+            case Verbosity::Quiet: { cmd.add("-q"); } break;
+            default: {} break;
+        }
+    }
+
+    cmd.add("-k"); /* Keep original */
+
+    if (out) {
+            "-c", /* Redirect to stdout */
+    }
+
+    cmd.add(compressed.string());
+
+    if (out) {
+            cmd.add(">", out->string()); /* Redirect to chosen file */
+    }
+    return cmd.run_sync() == 0;
+}
+
+bool extract(const fs::path& in,
+             std::optional<fs::path> out = std::nullopt,
+             std::optional<Verbosity> v = std::nullopt)
+{
+    /* Detect from extensions? or metadata? */
+
+    fs::path output = out.value_or(in.stem());
+
+    if (in.extension() == ".gz") {
+        fs::path no_gz = in.stem();
+        if (no_gz.extension() == ".tar") {
+            output = output.stem();
+            extract_tar_gz(in, output);
+        } else {
+            extract_gz(in, output);
+        }
+    } else if (in.extension() == ".zip") {
+        extract_zip(in, output);
+    } else if (in.extension() == ".bz2") {
+        fs::path no_bz2 = in.stem();
+        if (no_bz2.extension() == ".tar") {
+            extract_tar_bz2(in, output);
+        } else {
+            extract_bz2(in, output);
+        }
+    }
+}
+
+bool download_and_extract(const std::string& url, std::optional<fs::path> out = std::nullopt)
+{
+    throw std::runtime_error("TODO download_and_extract");
+}
 
 }
