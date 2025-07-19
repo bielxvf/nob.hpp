206b218edbf618968f1631d29a9b3073338ccb0b
Biel Sala Mimó <bielsalamimo@gmail.com>
2025-07-19 14:53:14 +0200

    Getting closer

--- nob.hpp
+++ nob.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <stdexcept>
 #include <filesystem>
+#include <fstream>
 #include <cstdio>
 #include <cstring>
 #include <iostream>
@@ -12,6 +13,7 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <limits.h>
+#include <optional>
 
 namespace {
 
@@ -20,7 +22,7 @@ std::string get_executable_path()
     char buf[PATH_MAX] = {0};
     ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf) - 1);
     if (len == -1) {
-        throw std::runtime_error("Failed to read /proc/self/exe");
+        throw std::runtime_error("get_executable_path(): Failed to read /proc/self/exe");
     }
     buf[len] = '\0';
     return std::string(buf);
@@ -77,7 +79,13 @@ void log(std::ostream& out, LogLevel lvl, Args&&... args)
     out << '\n';
 }
 
-inline bool mkdir(const fs::path& path)
+template<typename... Args>
+void error(Args&&... args)
+{
+    log(std::cerr, LogLevel::Error, std::forward<Args>(args)...);
+}
+
+bool mkdir(const fs::path& path)
 {
     if (fs::exists(path)) {
         if (fs::is_directory(path)) {
@@ -124,62 +132,77 @@ public:
         pid_t pid = fork();
 
         if (pid < 0) {
-            throw std::runtime_error(std::string("fork() failed: ") + std::strerror(errno));
+            throw std::runtime_error("run_sync(): fork() failed: " + std::string(std::strerror(errno)));
         } else if (pid == 0) {
             if (m_working_dir != ".") {
                 log(LogLevel::Info, "Changing working dir to ", m_working_dir);
                 fs::current_path(m_working_dir);
             }
             execvp(argv[0], argv.data());
-            perror("execvp failed");
+            perror("run_sync(): execvp failed");
             _exit(1);
         } else {
             int status;
             waitpid(pid, &status, 0);
-            return status;
+            if (WIFEXITED(status)) {
+                return WEXITSTATUS(status);
+            } else {
+                return 1;
+            }
         }
     }
 
-    std::string run_sync_capture()
+    /* TODO: handle stderr */
+    int run_sync_capture(std::ostream& out)
     {
         log(LogLevel::Info, "Running sync capture: ", *this);
+
         std::vector<char*> argv;
-        for (auto& s : m_command) {
+        for (auto s : m_command) {
             argv.push_back(s.data());
         }
         argv.push_back(nullptr);
 
         int pipefd[2];
         if (pipe(pipefd) == -1) {
-            throw std::runtime_error("pipe failed");
+            throw std::runtime_error("run_sync_capture(): pipe failed");
         }
 
         pid_t pid = fork();
         if (pid < 0) {
-            throw std::runtime_error(std::string("fork() failed: ") + std::strerror(errno));
+            throw std::runtime_error("run_sync_capture(): fork() failed: " + std::string(std::strerror(errno)));
         } else if (pid == 0) {
             close(pipefd[0]);
             dup2(pipefd[1], STDOUT_FILENO);
             close(pipefd[1]);
             execvp(argv[0], argv.data());
-            perror("execvp failed");
+            perror("run_sync_capture(): execvp failed");
             _exit(1);
         } else {
             close(pipefd[1]);
-            std::string output;
             char buffer[4096];
             ssize_t bytes_read;
-            while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) > 0) {
-                output.append(buffer, bytes_read);
-            }
-            if (bytes_read == -1) {
-                close(pipefd[0]);
-                throw std::runtime_error("Error reading from pipe");
+            while ((bytes_read = read(pipefd[0], buffer, sizeof(buffer))) != 0) {
+                if (bytes_read == -1) {
+                    if (errno == EINTR) {
+                        continue;
+                    } else {
+                        close(pipefd[0]);
+                        throw std::runtime_error("run_sync_capture(): Error reading from pipe");
+                    }
+                }
+                out.write(buffer, bytes_read);
             }
 
             int status;
             waitpid(pid, &status, 0);
-            return output;
+            close(pipefd[0]);
+            out.flush();
+            if (WIFEXITED(status)) {
+                return WEXITSTATUS(status);
+            } else {
+                return 1;
+            }
         }
     }
 
@@ -199,7 +222,7 @@ public:
 
 private:
     std::vector<std::string> m_command;
-    fs::path m_working_dir = ".";
+    fs::path m_working_dir { "." };
 };
 
 void go_rebuild_urself(int argc, char** argv, fs::path source_path)
@@ -213,7 +236,7 @@ void go_rebuild_urself(int argc, char** argv, fs::path source_path)
         log(LogLevel::Info, "Rebuilding meself");
         Cmd cmd("c++", source_path, "-o", binary_path);
         if (cmd.run_sync() != 0) {
-            throw std::runtime_error("Rebuild failed");
+            throw std::runtime_error("go_rebuild_urself(): Rebuild failed");
         }
         execvp(binary_path.c_str(), argv);
         perror("execvp failed");
@@ -233,11 +256,12 @@ bool download(const std::string& url,
         switch (*v) {
             case Verbosity::Verbose: { cmd.add("-v"); } break;
             case Verbosity::Quiet: { cmd.add("-s"); } break;
+            case Verbosity::Quieter: { cmd.add("-s"); } break;
             default: {} break;
         }
     }
 
-    if (out_path) {
+    if (out) {
         cmd.add("-o", out->string());
     } else {
         cmd.add("-O"); /* Save to whatever name is in the Content-Disposition header or the last part of the URL */
@@ -251,10 +275,10 @@ bool extract_tar_gz(const fs::path& archive,
                     std::optional<fs::path> out = std::nullopt,
                     std::optional<Verbosity> v = std::nullopt)
 {
-    Cmd cmd("tar",
+    Cmd cmd { "tar",
             "-x",
             "-z"
-    );
+    };
 
     if (v) {
         switch (*v) {
@@ -265,9 +289,9 @@ bool extract_tar_gz(const fs::path& archive,
 
     cmd.add("-f", archive.string());
 
-    if (out) {
-        cmd.add("-C", out->string());
-    }
+    // if (out) {
+    //     cmd.add("-C", out->string());
+    // }
 
     return cmd.run_sync() == 0;
 }
@@ -276,10 +300,10 @@ bool extract_tar_bz2(const fs::path& archive,
                      std::optional<fs::path> out = std::nullopt,
                      std::optional<Verbosity> v = std::nullopt)
 {
-    Cmd cmd("tar",
+    Cmd cmd { "tar",
             "-x", /* Extract */
             "-j"  /* Use bz2 decompression */
-    );
+    };
 
     if (v) {
         switch (*v) {
@@ -301,12 +325,7 @@ bool extract_bz2(const fs::path& compressed,
                  std::optional<fs::path> out = std::nullopt,
                  std::optional<Verbosity> v = std::nullopt)
 {
-    Cmd cmd("bzip2",
-            "-d"); /* Decompress */
-
-    if (out) {
-        cmd.add("-c"); /* To stdout */
-    }
+    Cmd cmd { "bzip2", "-d" }; /* Decompress */
 
     cmd.add("-k", compressed.string()); /* Keep original */
 
@@ -318,10 +337,19 @@ bool extract_bz2(const fs::path& compressed,
         }
     }
 
-    /* TODO: THIS WON'T WORK because this isnt a shell command its execvp, we probably need to cmd.run_syn_capture() */
-    /*       and then write to the output file ourselves */
     if (out) {
-        cmd.add(">", out->string()); /* Redirect to chosen file */
+        /* Redirect to stdout and capture */
+        cmd.add("-c");
+        /* TODO: Check error */
+        std::ofstream outfile;
+        outfile.open(out->string());
+        if (outfile.is_open()) {
+            int status = cmd.run_sync_capture(outfile);
+            outfile.close();
+            return status;
+        } else {
+            throw std::runtime_error("extract_bz2(): Could not open file " + out->string());
+        }
     }
 
     return cmd.run_sync() == 0;
@@ -331,7 +359,7 @@ bool extract_zip(const fs::path& archive,
                  std::optional<fs::path> out = std::nullopt,
                  std::optional<Verbosity> v = std::nullopt)
 {
-    Cmd cmd("unzip");
+    Cmd cmd { "unzip" };
 
     if (v) {
         switch (*v) {
@@ -345,10 +373,10 @@ bool extract_zip(const fs::path& archive,
     cmd.add(archive.string());
 
     if (out) {
-            cmd.add("-d", out->string()); /* Redirect to chosen file */
+        cmd.add("-d", out->string()); /* Redirect to chosen file */
     }
 
-    return cmd.run_syns() == 0;
+    return cmd.run_sync() == 0;
 }
 
 bool extract_gz(const fs::path& compressed,
@@ -367,17 +395,23 @@ bool extract_gz(const fs::path& compressed,
 
     cmd.add("-k"); /* Keep original */
 
-    if (out) {
-            "-c", /* Redirect to stdout */
-    }
-
     cmd.add(compressed.string());
 
-    /* TODO: THIS WON'T WORK because this isnt a shell command its execvp, we probably need to cmd.run_syn_capture() */
-    /*       and then write to the output file ourselves */
     if (out) {
-            cmd.add(">", out->string()); /* Redirect to chosen file */
+        /* Redirect to stdout and capture */
+        cmd.add("-c");
+        /* TODO: Check error */
+        std::ofstream outfile;
+        outfile.open(out->string());
+        if (outfile.is_open()) {
+            int status = cmd.run_sync_capture(outfile);
+            outfile.close();
+            return status;
+        } else {
+            throw std::runtime_error("extract_gz(): Could not open file " + out->string());
+        }
     }
+
     return cmd.run_sync() == 0;
 }
 
@@ -393,25 +427,39 @@ bool extract(const fs::path& in,
         fs::path no_gz = in.stem();
         if (no_gz.extension() == ".tar") {
             output = output.stem();
-            extract_tar_gz(in, output);
+            return extract_tar_gz(in, output, v);
         } else {
-            extract_gz(in, output);
+            return extract_gz(in, output, v);
         }
     } else if (in.extension() == ".zip") {
-        extract_zip(in, output);
+        return extract_zip(in, output, v);
     } else if (in.extension() == ".bz2") {
         fs::path no_bz2 = in.stem();
         if (no_bz2.extension() == ".tar") {
-            extract_tar_bz2(in, output);
+            return extract_tar_bz2(in, output, v);
         } else {
-            extract_bz2(in, output);
+            return extract_bz2(in, output, v);
         }
     }
+
+    return false;
 }
 
-bool download_and_extract(const std::string& url, std::optional<fs::path> out = std::nullopt)
+bool download_and_extract(const std::string& url,
+                          std::optional<fs::path> out = std::nullopt,
+                          std::optional<Verbosity> v = std::nullopt)
 {
-    throw std::runtime_error("TODO download_and_extract");
+    fs::path archive_path = fs::path(url.substr(url.find_last_of('/') + 1));
+
+    if (!download(url, archive_path, v)) {
+        return false;
+    }
+
+    if (out) {
+        mkdir(*out);
+    }
+
+    return extract(archive_path, out.value_or(archive_path.stem().stem()), v);
 }
 
 }
